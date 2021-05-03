be5957a4e1bf02e6bd74822f6d545825360b7bbf
Juan David Serrano <jd.serranom@uniandes.edu.co>
2021-05-03 00:27:33 -0500

    Modificaciones contacto para hacerlo funcional

--- src/components/ContactoViejo.js
+++ src/components/ContactoViejo.js
@@ -1,28 +1,30 @@
-import React, {useState} from 'react'
-import "./Contacto.css";
+import React, { Component } from 'react'
+import "./Contacto.css"; 
 import logo from "../recursos/palabraLogo.svg";
 import emailjs from 'emailjs-com';
 
-function Contacto(props){
+class ContactoViejo extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { mensaje: {
+            nombre: '',
+            correo: '',
+            tipo: 'No seleccionado',
+            mensaje: '',
+            mailSent: false,
+            error: null
+        }, show: false };
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
 
-    let [mensajeObj, setMensaje] = useState({
-        nombre: '',
-        correo: '',
-        tipo: 'No seleccionado',
-        mensaje: '',
-        mailSent: false,
-        error: null
-    });
-
-
-    function handleInputChange(event) {
-        setMensaje({...mensajeObj, [event.target.name]: event.target.value});
+    handleInputChange(event) {
+        this.setState({ mensaje: { ...this.state.mensaje, [event.target.name]: event.target.value } });
     }
 
 
-    function handleFormSubmit(e){
+    handleFormSubmit = e => {
         e.preventDefault();
-        const {mensaje, nombre, correo, tipo} = mensajeObj;
+        const {mensaje, nombre, correo, tipo} = this.state.mensaje;
         const params = {message: mensaje, name: nombre, email: correo, userType: tipo};
         emailjs.send('service_ruy2cos','template_l12eqwy', params, 'user_1AVCQW2D8N6FMw6LVfyvu')
             .then((response) => {
@@ -30,32 +32,33 @@ function Contacto(props){
             }, (err) => {
                 console.log('FAILED...', err);
             });
-    }
+    };
 
 
-    return (
+    render(){
+        return (
         <div className="Contacto" id="contacto">
-            <div className="form-container" onSubmit={e=>handleFormSubmit(e)}>
+            <div className="form-container">
                 <form className="contacto-form" action="/mail.php">
                     <div className="contacto-form-primera">
                         <div>
                             <p>NOMBRE</p>
-                            <input type="text" id="inputText1" name="nombre"  onChange={handleInputChange}/>
+                            <input type="text" id="inputText1" name="nombre"  onChange={this.handleInputChange}/>
                         </div>
                         <div>
                             <p>EMAIL</p>
-                            <input type="text" id="inputText2" name="correo" onChange={handleInputChange}/>
+                            <input type="text" id="inputText2" name="correo" onChange={this.handleInputChange}/>
                         </div>
                     </div>
                     <p>TIPO DE USUARIO</p>
-                    <select id="inputState" className="form-control" name="tipo" defaultValue={'DEFAULT'} onChange={handleInputChange}>
+                    <select id="inputState" className="form-control" name="tipo" defaultValue={'DEFAULT'} onChange={this.handleInputChange}>
                         <option value="DEFAULT" disabled></option>
                         <option value="Emprendimiento">Quiero publicar mi marca</option>
                         <option value="Usuario">Estoy interesado en comprar</option>
                     </select>
                     <p>MENSAJE</p>
-                    <textarea className="form-control" id="textArea" rows="2" name="mensaje" onChange={handleInputChange}/>
-                    <button className="botonForm" type="submit">
+                    <textarea className="form-control" id="textArea" rows="2" name="mensaje" onChange={this.handleInputChange}/>
+                    <button className="botonForm" type="submit" onClick={e => this.handleFormSubmit(e)}>
                         <span>ENVIAR</span>
                     </button>
                 </form>
@@ -64,10 +67,11 @@ function Contacto(props){
                 <h3>¿Quieres <span>formar</span> parte de <img className="img" src={logo} alt="ITTI" /> ?</h3>
                 <p>¡Si tienes una marca que quieres potenciar, si quieres encontrar regalos fácilmente o si simplemente eres un apasionado del talento local, déjanos tus datos y nos pondremos en contacto!</p>
             </div>
-
+            
         </div>
-    )
-
+        )
+    }
+    
 }
 
-export default Contacto
+export default ContactoViejo
